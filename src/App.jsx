d5f4c3d6e4e55a70bf89a3b965910d4fc4b609a5
Rasel Sharif <raselsharif9@gmail.com>
2023-08-01 17:29:23 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './assets/components/Navbar';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500 text-5xl'>hello</h1>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <div className='text-red-500 text-4xl'>Navbar</div>
+    <div className=' text-2xl text-white py-3 mt-4 bg-slate-950'>Navbar 2</div>
+    </>
+  )
+}
+
+export default Navbar
